52c491366037e9c6cda6e54ab92729191c97b466
jcorp11 <joaco.011@gmail.com>
2024-03-12 19:05:35 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import { leer, registrar } from "./operaciones.js";
+
+const arg = process.argv.slice(2);
+const [operacion, ...params] = arg;
+// console.log(arg);
+// console.log(params);
+
+const [nombre, edad, tipo, color, enfermedad] = params;
+// console.log({ nombre, edad, tipo, color, enfermedad });
+
+const main = async () => {
+  switch (operacion) {
+    case "registrar":
+      await registrar(nombre, edad, tipo, color, enfermedad);
+      break;
+    case "leer":
+      await leer();
+      break;
+    default:
+      console.log("Unknown option:", operacion);
+  }
+};
+
+main();
+// node index.js registrar Benito "2 años" perro blanco vomitos
+//node index.js leer
---
+++ operaciones.js
@@ -0,0 +1,41 @@
+import { readFile, writeFile } from "fs/promises";
+
+async function read() {
+  try {
+    const data = await readFile("citas.json", "utf-8");
+    return JSON.parse(data);
+  } catch (error) {
+    await escribir([]);
+    console.log(error);
+    return [];
+  }
+}
+
+async function escribir(citas) {
+  try {
+    await writeFile("citas.json", JSON.stringify(citas));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function registrar(nombre, edad, tipo, color, enfermedad) {
+  const citas = await read();
+  const nuevaCita = {
+    nombre,
+    edad,
+    tipo,
+    color,
+    enfermedad,
+  };
+  citas.push(nuevaCita);
+  await escribir(citas);
+  console.log("Se ha registrado la nueva cita");
+}
+
+async function leer() {
+  const citas = await read();
+  console.log(citas);
+}
+
+export { registrar, leer };
